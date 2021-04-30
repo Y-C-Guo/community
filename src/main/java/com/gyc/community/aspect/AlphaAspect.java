25732697ec2f0de77f60a10f95bfef13fbdfc3c1
guoyichen <905956068@qq.com>
2021-04-30 10:42:00 +0800

    统一记录日志AOP

---
+++ src/main/java/com/gyc/community/aspect/AlphaAspect.java
@@ -0,0 +1,56 @@
+package com.gyc.community.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class AlphaAspect {
+
+    //切点
+    @Pointcut("execution(* com.gyc.community.service.*.*(..))")
+    public void pointcut(){
+
+    }
+
+
+    //通知
+    @Before("pointcut()")
+    public void before(){
+        System.out.println("before");
+    }
+
+    @After("pointcut()")
+    public void after(){
+        System.out.println("after");
+    }
+    @AfterReturning("pointcut()")
+    public void afterReturning(){
+        System.out.println("afterReturning");
+    }
+
+    @AfterThrowing("pointcut()")
+    public void afterThrowing(){
+        System.out.println("afterThrowing");
+    }
+
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable{
+        System.out.println("aroundBefore");
+        Object proceed = joinPoint.proceed();
+        System.out.println("aroundAfter");
+        return proceed;
+
+    }
+
+
+
+
+
+
+
+
+
+}
